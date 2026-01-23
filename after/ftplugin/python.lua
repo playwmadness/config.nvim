2bfc0a8f475f8ef9db7ded5c1e8856bc34ac2de7
Vaiva Kazanaviciute <me@pwmstuff.com>
2026-01-23 17:38:53 +0100

    format python post-write

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -3,5 +3,5 @@ set.lcs = "space:•,eol:$,tab:>-,leadmultispace:|   ,multispace:....•"
 set.fdl = 1
 
 vim.cmd [[
-  au BufWritePre *.py,*.pyi silent! !uvx ruff format %
+  au BufWritePost *.py,*.pyi silent! !uvx ruff format %
 ]]
